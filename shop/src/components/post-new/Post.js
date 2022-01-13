56850e05b6caca287fbaeaf294c0abffe18e3cb2
tomnyson <tabletkindfire@gmail.com>
2022-01-13 20:22:31 +0700

    crud api

--- shop/src/components/post-new/Post.js
+++ shop/src/components/post-new/Post.js
@@ -3,9 +3,11 @@ import React from 'react';
 import styled from 'styled-components'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton'
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
 const Post = ({picture, name,description, id, createdAt, onRemove, onEdit}) => {
-
   const onHandleEdit = (item) => {
     onEdit(item)
   }
@@ -22,9 +24,12 @@ const Post = ({picture, name,description, id, createdAt, onRemove, onEdit}) => {
         <h3>{name}</h3>
         <span style={{fontSize: '10px', color: 'gray'}}>{createdAt}</span>
         <p>{description}</p>
-        <button onClick={() => onHandleEdit({
+        <IconButton onClick={() => onHandleEdit({
           picture, name,description, id
-        })}>EDIT</button>
+        })}><EditOutlinedIcon/></IconButton>
+        <IconButton onClick={() => onHandleEdit({
+          picture, name,description, id
+        })}><DeleteOutlineOutlinedIcon/></IconButton>
       </Grid>
     </Grid>
   )
